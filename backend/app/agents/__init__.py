56649ffbb4df91fe952b83125cca32e6d981c844
Tekleab15 <teklishgmn15gmail.com>
2026-07-09 20:57:55 +0000

    Adjusting the symbio-ai file structure to align with Track-1

--- backend/app/agents/__init__.py
+++
@@ -1,7 +0,0 @@
-from .field_intake import FieldIntakeAgent
-from .vision_analysis import VisionAnalysisAgent
-from .agronomy_knowledge import AgronomyKnowledgeAgent
-from .rule_compliance import RuleComplianceAgent
-from .neuro_symbolic_auditor import NeuroSymbolicAuditorAgent
-from .operations_report import OperationsReportAgent
-from .supervisor import SupervisorAgent
